ee2229e7490277eac9286f788dd00fe64b93fc2d
upeksu <upe.kennesaw@gmail.com>
2025-01-16 16:07:00 -0500

    code snippets

---
+++ Module1.py
@@ -0,0 +1,9 @@
+# prints "Hello World" to the console
+print("Hello World")
+print("I have 5 dogs")
+
+# storing information into a variable
+name = "John Doe" # string data type
+age = 65 # int data type
+height = 182.88 # float data type
+likesCandy = True # boolean data type
