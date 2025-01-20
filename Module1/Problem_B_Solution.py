61c69ae3ab3eef19b70ceae7dfbc6e811320faf1
Wyatt Bramblett <wbramblett1@gmail.com>
2025-01-20 18:06:55 -0500

    Finnished mod1 and examples

---
+++ Module1/Problem_B_Solution.py
@@ -0,0 +1,35 @@
+# There are many other ways to do this, 
+
+#----------------------------------------------------
+
+birthYear = int(input("What year were you born: "))
+currentYear = int(input("What year is it: "))
+
+age = currentYear - birthYear
+
+print(f"Your current age is: {age}")
+
+#----------------------------------------------------
+
+birthYear = int(input("What year were you born: "))
+currentYear = int(input("What year is it: "))
+
+print(f"Your current age is: {currentYear - birthYear}")
+
+#----------------------------------------------------
+
+birthYear = int(input("What year were you born: "))
+currentYear = int(input("What year is it: "))
+
+print("Your current age is:", currentYear - birthYear)
+
+#----------------------------------------------------
+
+
+birthYear = input("What year were you born: ")
+currentYear = input("What year is it: ")
+
+print("Your current age is:", int(currentYear) - int(birthYear))
+
+#----------------------------------------------------
+
