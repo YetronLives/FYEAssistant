02e625428c3b620207a30680c3c93058dbe1af87
YetronLives <cnnorteyjr@gmail.com>
2025-02-03 14:47:34 -0500

    m2 update

--- Module2/Module2.py
+++ Module2/Module2.py
@@ -40,3 +40,23 @@ else:
 
 
 #MATCH CASE
+color = input ("Enter your favorite color: ") # test this code in your IDE
+
+# It will print out one of these cases based on what you type
+match color:
+    case "red":
+        print("Ouu that's a hot one")
+    case "orange":
+        print("I hope you meant the color haha")
+    case "yellow":
+        print("Mmm, reminds me of mustard!")
+    case "green":
+        print("You know what that means, GO, GO, GO!")
+    case "blue":
+        print("That's my favorite color too! Isn't the sky so beautiful?")
+    # if you input does not match any of the cases above,
+    # it will go to this case, which is the default
+    case _:
+        print("That's a good color!")
+    
+    
